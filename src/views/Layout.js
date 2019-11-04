9c614785fbc82a16479d188c4871b93c67f4310b
Mikayda Mills <mills.ma12@gmail.com>
2019-11-03 21:54:39 -0500

    add skeleton for main components;

---
+++ src/views/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const LayoutContainer = styled.div``;
+
+function Layout() {
+  return <LayoutContainer></LayoutContainer>;
+}
+
+export default Layout;
---
+++ src/views/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProjectsContainer = styled.div``;
+
+function Projects() {
+  return <ProjectsContainer></ProjectsContainer>;
+}
+
+export default Projects;
