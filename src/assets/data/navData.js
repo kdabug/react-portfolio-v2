f05259865f1bfb046da3c5ad51029345fb44c84e
Mikayda Mills <mills.ma12@gmail.com>
2019-11-05 15:59:04 -0500

    renders with puppy pic

--- src/assets/data/navData.js
+++ src/assets/data/navData.js
@@ -2,7 +2,7 @@ import MePic from "../photos/surf_mikayda.jpg";
 import BradyPic from "../photos/brady.jpg";
 import DallinPic from "../photos/dallin_mikayda.jpg";
 
-export const { navData } = [
+export const navData = [
   { name: "contact", photo: BradyPic, alt: "Brady the Labradoodle" },
   { name: "projects", photo: DallinPic, alt: "My Family" },
   { name: "home", photo: MePic, alt: "Wake Surfing" }
